2507f7471cc5f0e98eccb11877304e6140d602e9
Vishnu Sah <vishnusah449@gmail.com>
2024-01-04 16:26:01 +0530

    add css and made UI better

--- todo_app/tasks/forms.py
+++ todo_app/tasks/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    task= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
     class Meta:
         model = Task
         fields = '__all__'
